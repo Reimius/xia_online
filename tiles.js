0bbaff57277c3c258d0b557f976886556e1cf6a7
Paul Schroeder <paul.f.schroeder@hotmail.com>
2015-04-14 17:55:14 -0500

    Added in more tile connection symbol renders

--- tiles.js
+++ tiles.js
@@ -279,11 +279,53 @@ Xia.Tile = new JS.Class({
 			c2.strokeStyle = "#FFFF00";
 			c2.closePath();
 			c2.stroke();
-			debugger;
 		},
-		"2" : function(){},
-		"3" : function(){},
-		"4" : function(){},
+		"2" : function(x, y, rotations){
+			var c2 = Xia.canvas.c2;
+			c2.beginPath();
+			c2.moveTo(x, y);
+			var deltas = Xia.rotateAroundOrigin(0, (Xia.hex.canvasHexHeight / 6), rotations);
+			c2.lineTo(x + deltas.x, y + deltas.y);
+			c2.strokeStyle = "#FFFF00";
+			deltas = Xia.rotateAroundOrigin((Xia.hex.canvasHexHeight / 15), (Xia.hex.canvasHexHeight / 6), rotations);
+			c2.moveTo(x + deltas.x, y + deltas.y);
+			deltas = Xia.rotateAroundOrigin(-(Xia.hex.canvasHexHeight / 15), (Xia.hex.canvasHexHeight / 6), rotations);
+			c2.lineTo(x + deltas.x, y + deltas.y);
+			c2.closePath();
+			c2.stroke();
+		},
+		"3" : function(x, y, rotations){
+			var c2 = Xia.canvas.c2;
+			c2.beginPath();
+			c2.moveTo(x, y);
+			var deltas = Xia.rotateAroundOrigin(0, (Xia.hex.canvasHexHeight / 6), rotations);
+			c2.lineTo(x + deltas.x, y + deltas.y);
+			c2.strokeStyle = "#FFFF00";
+			deltas = Xia.rotateAroundOrigin((Xia.hex.canvasHexHeight / 20), (Xia.hex.canvasHexHeight / 6), rotations);
+			c2.moveTo(x + deltas.x, y + deltas.y);
+			deltas = Xia.rotateAroundOrigin(-(Xia.hex.canvasHexHeight / 20), (Xia.hex.canvasHexHeight / 6), rotations);
+			c2.lineTo(x + deltas.x, y + deltas.y);
+			deltas = Xia.rotateAroundOrigin(-(Xia.hex.canvasHexHeight / 20), (Xia.hex.canvasHexHeight / 4), rotations);
+			c2.lineTo(x + deltas.x, y + deltas.y);
+			deltas = Xia.rotateAroundOrigin((Xia.hex.canvasHexHeight / 20), (Xia.hex.canvasHexHeight / 4), rotations);
+			c2.lineTo(x + deltas.x, y + deltas.y);
+			c2.closePath();
+			c2.stroke();
+		},
+		"4" : function(x, y, rotations){
+			var c2 = Xia.canvas.c2;
+			c2.beginPath();
+			c2.moveTo(x, y);
+			var deltas = Xia.rotateAroundOrigin(0, (Xia.hex.canvasHexHeight / 6), rotations);
+			c2.lineTo(x + deltas.x, y + deltas.y);
+			c2.strokeStyle = "#FFFF00";
+			c2.closePath();
+			c2.stroke();
+			c2.beginPath();
+			deltas = Xia.rotateAroundOrigin(0, (Xia.hex.canvasHexHeight / 6) + (Xia.hex.canvasHexHeight / 20), rotations);
+			c2.arc(x + deltas.x,y + deltas.y, (Xia.hex.canvasHexHeight / 20),0*Math.PI,2*Math.PI);
+			c2.stroke();
+		},
 		"5" : function(){},
 		"6" : function(){}
 	},
